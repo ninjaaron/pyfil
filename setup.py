12ebf2dd9eb0cecea9b1c00f814a54d4278373cd
Aaron Christianson <ninjaaron@gmail.com>
2016-09-03 14:29:59 -0500

    added a link to the git repo in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Aaron Christianson',
     license='BSD',
     author_email='ninjaaron@gmail.com',
-    # url='https://github.com/ninjaaron/???',
+    url='https://github.com/ninjaaron/pyfil'
     description='print the return value of python expression -- also in loops',
     long_description=open('README.rst').read(),
     keywords='evaluate',
