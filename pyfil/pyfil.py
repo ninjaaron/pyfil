1fd9fee9016fd61a40688992718343160ddbc548
Aaron Christianson <ninjaaron@gmail.com>
2016-09-04 17:57:50 -0500

    add exception handling

--- pyfil/pyfil.py
+++ pyfil/pyfil.py
@@ -78,8 +78,10 @@ import sys
 import json
 import os
 import re
-from . import env
+import ast
+import builtins
 from functools import update_wrapper
+from . import env
 
 
 class reify(object):
@@ -127,24 +129,46 @@ class SafeList(collections.UserList):
         return iter(self.data)
 
 
+def handle_errors(exception, args):
+    if args.raise_errors:
+        raise exception
+    elif args.silence_errors:
+        return
+    else:
+        print('\x1b[31mError\x1b[0m:', exception)
+        return
+
+
 def run(expressions, args, namespace={}):
     func = exec if args.quiet else eval
     for expr in expressions:
-        try:
-            value = func(expr, namespace)
-        except Exception as e:
-            if args.throw_errors:
-                raise e
-            elif args.silence_errors:
+        if args.exception_handler:
+            exception, handler = tuple(
+                    i.strip() for i in
+                    args.exception_handler.split(':', maxsplit=1))
+            try:
+                value = func(expr, namespace)
+            except builtins.__dict__[exception]:
+                try:
+                    value = func(handler, namespace)
+                except Exception as e:
+                    handle_errors(e, args)
+                    continue
+            except Exception as e:
+                handle_errors(e, args)
                 continue
-            else:
-                print('\x1b[31mError\x1b[0m:', e)
+        else:
+            try:
+                value = func(expr, namespace)
+            except Exception as e:
+                handle_errors(e, args)
                 continue
 
         if not args.quiet:
-            if args.join and isinstance(value, collections.Iterable):
-                print(eval("'"+args.join.replace("'", r"\'")+"'",
-                    {}).join(value))
+            if args.join is not None and isinstance(value,
+                                                    collections.Iterable):
+                print(ast.literal_eval("'''" + args.join.replace("'", r"\'") +
+                    "'''").join(value))
             elif isinstance(value, collections.Iterator):
                 for i in value:
                     print(i)
@@ -180,11 +204,14 @@ def main():
                           "implies -l")
     ap.add_argument('-n', '--join', metavar='STRING',
                     help='join items in iterables with STRING')
-    ap.add_argument('-E', '--throw-errors', action='store_true',
+    ap.add_argument('-R', '--raise-errors', action='store_true',
                     help='raise errors and in evaluation and stop execution '
                          '(default: print message to stderr and continue)')
     ap.add_argument('-S', '--silence-errors', action='store_true',
                     help='suppress error messages')
+    ap.add_argument('-H', '--exception-handler',
+                    help='specify exception handler with the format '
+                         '`Exeption: alternative expression to eval`')
 
     a = ap.parse_args()
 
