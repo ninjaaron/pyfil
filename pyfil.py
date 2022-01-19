32a6add63625fc47735333bed63db64276291bea
e2dubba <svirgilgoodwin@gmail.com>
2022-01-18 18:34:55 -0700

    Updating to Collections.abc Iterable
    * Python 3.10 moves `collections.Iterator` to `collections.abc.Iterable`

--- pyfil.py
+++ pyfil.py
@@ -39,6 +39,7 @@ import re
 import ast
 import argparse
 from functools import update_wrapper
+from collections.abc import Iterable
 
 
 class LazyDict(dict):
@@ -168,7 +169,7 @@ def run(expressions, args, namespace={}):
             namespace.update(x=value)
 
     if not (args.quiet or args.exec):
-        if args.join is not None and isinstance(value, collections.Iterable):
+        if args.join is not None and isinstance(value, Iterable):
             print(
                 ast.literal_eval("'''" + args.join.replace("'", r"\'") + "'''").join(
                     map(str, value)
@@ -176,7 +177,7 @@ def run(expressions, args, namespace={}):
             )
         elif value is None:
             pass
-        elif isinstance(value, collections.Iterator):
+        elif isinstance(value, Iterable):
             for i in value:
                 print_obj(i)
         else:
